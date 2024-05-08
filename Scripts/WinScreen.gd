e793a9545ecf7d013e97ec2dc3b0156a38400ae8
Blurry <julian.lb.games@gmail.com>
2024-05-08 22:49:25 +0200

    Lesson23: Start Menu

--- Scripts/WinScreen.gd
+++ Scripts/WinScreen.gd
@@ -1,4 +1,4 @@
 class_name WinScreen extends Control
 
 func _on_button_pressed()-> void:
-	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
