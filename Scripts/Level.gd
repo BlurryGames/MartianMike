27fd06ef2f4b106e4f4be189cea1850a0b7b9702
Blurry <julian.lb.games@gmail.com>
2024-05-08 17:11:33 +0200

    Lesson7: Deathzone

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,7 +1,13 @@
 class_name Level extends Node2D
 
+@onready var startPosition: Marker2D = $StartPosition
+
 func _process(_delta: float)-> void:
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
+
+func _on_deathzone_body_entered(body: Node2D)-> void:
+	body.velocity = Vector2.ZERO
+	body.global_position = startPosition.global_position
